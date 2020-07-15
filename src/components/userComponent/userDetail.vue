6b590f58d968ab5df03005f3dad499c066ef932c
“Vimalate <1902644369@qq.com>
2020-07-15 22:43:02 +0800

    userDetail

---
+++ src/components/userComponent/userDetail.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: Vimalakirti
+ * @Date: 2020-07-15 22:40:50
+ * @LastEditTime: 2020-07-15 22:40:51
+ * @Description: 
+ * @FilePath: \bilibili\bilibili\src\components\userComponent\userDetail.vue
+--> 
+<template>
+  <div>userDetail</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
--- src/views/userinfo.vue
+++ src/views/userinfo.vue
@@ -1,26 +1,42 @@
 <!--
  * @Author: Vimalakirti
  * @Date: 2020-07-14 21:47:40
- * @LastEditTime: 2020-07-14 21:51:48
+ * @LastEditTime: 2020-07-15 22:42:26
  * @Description: 
  * @FilePath: \bilibili\bilibili\src\views\userinfo.vue
 --> 
 <template>
-  <div>
+  <div class="userinfo">
     <NavBar />
+    <div>
+      <img
+        class="backImg"
+        src="http://s1.hdslb.com/bfs/static/jinkela/mstation-h5/asserts/bannerTop_new.png"
+        alt
+      />
+    </div>
+    <UserDetail />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/Navbar.vue";
+import UserDetail from "@/components/userComponent/userDetail.vue";
 export default {
   data() {
     return {};
   },
   components: {
-    NavBar
+    NavBar,
+    UserDetail
   }
 };
 </script>
 <style lang='scss' scoped>
+.userinfo {
+  .backImg {
+    width: 100%;
+    height: 91px;
+  }
+}
 </style>
