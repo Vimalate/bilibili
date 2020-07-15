0899303678039105d557532d5db26a49fc7a5b05
“Vimalate <1902644369@qq.com>
2020-07-15 23:27:15 +0800

    userDetail

--- src/components/userComponent/userDetail.vue
+++ src/components/userComponent/userDetail.vue
@@ -1,12 +1,39 @@
 <!--
  * @Author: Vimalakirti
  * @Date: 2020-07-15 22:40:50
- * @LastEditTime: 2020-07-15 22:40:51
+ * @LastEditTime: 2020-07-15 23:26:29
  * @Description: 
  * @FilePath: \bilibili\bilibili\src\components\userComponent\userDetail.vue
 --> 
 <template>
-  <div>userDetail</div>
+  <div class="userDetail">
+    <div>
+      <div>
+        <img src="http://i1.hdslb.com/bfs/face/e8e44b9e8611f76a3c4d71e6750d1c8c74bf3bef.jpg" alt />
+      </div>
+      <div class="user_edit">
+        <div class="user_edit_detail">
+          <p>
+            <span>269</span>
+            <span class="user_text">粉丝</span>
+          </p>
+          <p>
+            <span>101</span>
+            <span class="user_text">关注</span>
+          </p>
+          <p>
+            <span>121</span>
+            <span class="user_text">获赞</span>
+          </p>
+        </div>
+        <div class="user_btn">编辑资料</div>
+      </div>
+    </div>
+    <div class="username">
+      <div>雨落如天</div>
+      <p>这个人很神秘，什么都没有写</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,4 +44,55 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
+.userDetail {
+  background-color: #fff;
+  padding: 2px 12px;
+  > div {
+    display: flex;
+  }
+  img {
+    height: 85px;
+    width: 85px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+  .user_edit {
+    flex: 1;
+    .user_edit_detail {
+      display: flex;
+      margin-top: 10px;
+      p {
+        flex: 1;
+        display: flex;
+        justify-self: center;
+        align-items: center;
+        flex-direction: column;
+        font-size: 13px;
+        .user_text {
+          color: #ccc;
+        }
+      }
+    }
+    .user_btn {
+      border: 1px solid #fb7299;
+      padding: 5px;
+      display: flex;
+      text-align: center;
+      justify-content: center;
+      margin-top: 8px;
+      color: #fb7299;
+      border-radius: 3px;
+    }
+  }
+  .username {
+    display: flex;
+    flex-direction: column;
+    padding-top: 10px;
+    p {
+      font-size: 12px;
+      color: #999;
+      padding: 10px 0;
+    }
+  }
+}
 </style>
